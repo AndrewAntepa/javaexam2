4816e60e522837d3476a5ea315c9ca0ed83847f7
AndrewAntepa <andrewantepa@gmail.com>
2025-02-01 00:18:21 +0800

    initial

---
+++ javaExam/src/main/java/org/skypro/JavaExam/javaExam/JavaExamApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.JavaExam.javaExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExamApplication.class, args);
+	}
+
+}
